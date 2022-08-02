1812f9ad0f1bd6e105b089df7fd2a4917208d5da
Thanh Luu <genghisk2000@gmail.com>
2022-08-02 23:00:30 +0700

    Add API to admin manage job, company

--- src/app/_services/admin.service.ts
+++ src/app/_services/admin.service.ts
@@ -1,9 +1,72 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
+  readonly APIUrl = "https://localhost:5001/api";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // ---- Company -----
+  getCompanyList(): Observable<any[]> {
+    return this.http.get<any>(this.APIUrl + '/Company/GetCompanyList');
+  }
+
+  addCompany(val: any) {
+    return this.http.post(this.APIUrl + '/Company/AddCompany', val);
+  }
+
+  // update có thêm 1 tham số id ở cuối url
+  updateCompany(val: any) {
+    return this.http.put(this.APIUrl + '/Company/UpdateCompany/', val);
+  }
+
+  // chưa có API
+  deleteCompany(val: any) {
+    return this.http.delete(this.APIUrl + '' + val);
+  }
+
+
+  // ---- Job -----
+  getJobList(): Observable<any[]> {
+    return this.http.get<any>(this.APIUrl + '/Job/GetJobList');
+  }
+
+  addJob(val: any) {
+    return this.http.post(this.APIUrl + 'Job/PostNewJob/register', val);
+  }
+
+  // update có thêm 1 tham số id ở cuối url
+  updateJob(val: any) {
+    return this.http.put(this.APIUrl + '/Job/UpdateJob/', val);
+  }
+
+  // chưa có API
+  deleteJob(val: any) {
+    return this.http.delete(this.APIUrl + '' + val);
+  }
+
+  // ---- Semester -----
+  getSemesterList(): Observable<any[]> {
+    return this.http.get<any>(this.APIUrl + '/Semester/GetSemesterList');
+  }
+
+  addSemester(val: any) {
+    return this.http.post(this.APIUrl + '/Semester/AddNewSemester/add', val);
+  }
+
+  // update có thêm 1 tham số id ở cuối url
+  updateSemester(val: any) {
+    return this.http.put(this.APIUrl + '/Semester/UpdateSemester/', val);
+  }
+
+  // delete có thêm 1 tham số id ở cuối url
+  deleteSemester(val: any) {
+    return this.http.delete(this.APIUrl + '/Semester/DeleteSemester/' + val);
+  }
+
+
 }
--- src/app/components/admin-components/admin-semesters/admin-semesters.component.ts
+++ src/app/components/admin-components/admin-semesters/admin-semesters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from 'src/app/_services/admin.service';
 
 @Component({
   selector: 'app-admin-semesters',
@@ -6,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminSemestersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: AdminService) { }
+
+  SemesterList: any = [];
 
   ngOnInit(): void {
+    this.refreshSemesterList();
+  }
+
+  refreshSemesterList() {
+    this.service.getSemesterList().subscribe(data => {
+      this.SemesterList = data;
+    });
   }
 
 }
